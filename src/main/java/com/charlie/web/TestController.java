78b668746ddbf196c6b5d7861cdd8ba3e37926a0
charlie <1151832099@qq.com>
2016-12-12 16:00:13 +0800

    a

---
+++ src/main/java/com/charlie/web/TestController.java
@@ -0,0 +1,21 @@
+package com.charlie.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class TestController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TestController.class, args);
+    }
+}
